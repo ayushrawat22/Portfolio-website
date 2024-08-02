84922534c2c31593dda31ddf387547955b46d13f
Ayush Rawat <ayushrawat271@gmail.com>
2024-08-02 18:45:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    var typed = new Typed('#element', {
+        strings: ['Software Engineer', 'Web Developer', 'Video Editor'],
+        typeSpeed: 60,
+    });
+});
