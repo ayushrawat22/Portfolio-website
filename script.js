72dca33c2fc3316a3e133d8f84b6ad96359b48b3
Ayush Rawat <151991433+ayushrawat22@users.noreply.github.com>
2024-08-02 19:12:38 +0530

    script.js

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
         strings: ['Software Engineer', 'Web Developer', 'Video Editor'],
         typeSpeed: 60,
     });
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navUl = document.querySelector('nav ul');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    menuToggle.addEventListener('click', () => {
+        navUl.classList.toggle('show');
+    });
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navUl.classList.remove('show');
+        });
+    });
 });
